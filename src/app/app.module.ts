4cfcea050944cd16b515e5c604186942fd305d34
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-06-03 01:38:59 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CdkDrag, CdkDragHandle} from '@angular/cdk/drag-drop';
+import { MsnApp } from './msn-app/msn-app.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MsnApp
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CdkDrag,
+    CdkDragHandle
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/msn-app/msn-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-msn-app',
+  templateUrl: './msn-app.component.html',
+  styleUrl: './msn-app.component.css'
+})
+export class MsnApp {
+
+}
