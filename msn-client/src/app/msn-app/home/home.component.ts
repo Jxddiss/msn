2a2f6771349365f013f693415b075c6e0ca3d7ba
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-06-05 13:54:25 -0400

    position home component animée after view init

--- msn-client/src/app/msn-app/home/home.component.ts
+++ msn-client/src/app/msn-app/home/home.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
 
+  constructor() { }
+
+  ngOnInit(): void {  
+    
+  }
+
+  ngAfterViewInit(): void {
+    this.positionAnimation()
+  }
+
+  positionAnimation() {
+    const tl = gsap.timeline();
+
+    tl.to('.window', {
+      top: '50%',
+      left: '20%',
+    })
+  }
+
+  ngOnDestroy(): void {
+  }
 }
