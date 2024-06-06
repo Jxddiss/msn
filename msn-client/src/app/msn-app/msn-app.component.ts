17138d49cad023c1c540de180726758bf737949e
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-06-05 20:17:26 -0400

    decouplage animation

--- msn-client/src/app/msn-app/msn-app.component.ts
+++ msn-client/src/app/msn-app/msn-app.component.ts
@@ -45,53 +45,52 @@ export class MsnApp implements AfterViewInit, OnDestroy{
   minimizeOrResume(){
     if(this.isMinimized){
       this.isMinimized = false
-      this.apparition()
+      this.apparition('.msn-window')
     }else{
-      this.disparition()
+      this.disparition('.msn-window')
       this.isMinimized = true
     }
-    
   }
 
   ngOnDestroy(){
     this._subscription.forEach(sub => sub.unsubscribe())
   }
 
-  apparition() : void{
+  apparition(sel : string = '.window') : void{
     const tl = gsap.timeline()
 
-    gsap.to('.window',{display:'block',opacity:1,})
+    gsap.to(sel,{display:'block',opacity:1,})
     
-    tl.from('.content-container',{
+    tl.from(sel+' .content-container',{
       height: 0,
       width:0,
       opacity:0,
     })
 
-    tl.from('.content-card',{
+    tl.from(sel+' .content-card',{
       opacity:0,
-    }).to('.content-card',{
+    }).to(sel+' .content-card',{
       opacity:1,
     })
-    tl.to(".content-container", {clearProps:true})
+    tl.to(sel+" .content-container", {clearProps:true})
     tl.duration(1)
     
   }
 
-  disparition() : void{
+  disparition(sel : string = '.window') : void{
     const tl = gsap.timeline()
 
-    tl.to('.content-card',{
+    tl.to(sel+' .content-card',{
       opacity:0,
     },0)
 
-    tl.to('.content-container',{
+    tl.to(sel+' .content-container',{
       width: '50px',
       height: '100px',
     })
 
-    tl.to('.window',{display:'none',opacity:0,})
-    tl.to(".content-container", {clearProps:true})
+    tl.to(sel,{display:'none',opacity:0,})
+    tl.to(sel+" .content-container", {clearProps:true})
 
     tl.duration(0.2)
   }
