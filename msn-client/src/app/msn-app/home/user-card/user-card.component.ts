5ee6e5887f718b9357d59dd109e53329a3f3cc02
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-06-09 14:53:26 -0400

    ajout changement photo dans le home

--- msn-client/src/app/msn-app/home/user-card/user-card.component.ts
+++ msn-client/src/app/msn-app/home/user-card/user-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-user-card',
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class UserCardComponent {
 
+  @ViewChild('avatarPicker') avatarPicker !: ElementRef
+  @ViewChild('avatarImg') avatarImg !: ElementRef
+
+  onChooseAvatar(){
+    this.avatarPicker.nativeElement.click()
+  }
+
+  onAvatarImgChange(event : Event){
+    const target = event.target as HTMLInputElement;
+    if (target && target.files && target.files.length > 0) {
+      const file = target.files[0];
+      let result = this.verifyFile(file);
+      if(result === "bon"){
+        const reader = new FileReader();
+        reader.onload = () => {
+          this.avatarImg.nativeElement.src = reader.result as string;
+        };
+        reader.readAsDataURL(file);
+      }else{
+        alert(result);
+        this.avatarPicker.nativeElement.value = '';
+      }
+    }
+  }
+
+  verifyFile(file: File) : string{ 
+    if(file.type === 'image/png' 
+    || file.type === 'image/jpg' 
+    || file.type === 'image/jpeg' 
+    || file.type === 'image/gif'){
+      if(file.size < 5000000){
+        return "bon"
+      }else{
+        return "Image trop lourde"
+      }
+    }
+
+    return "Format d'image non supportée"
+  }
 }
