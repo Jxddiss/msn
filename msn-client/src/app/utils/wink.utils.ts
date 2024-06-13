9529f22381da74404fdd6637b2f87fb31e65afa5
Nicholson Rainville Jacques <nics.rj.drawings@gmail.com>
2024-06-13 15:58:18 -0400

    ajout worship

--- msn-client/src/app/utils/wink.utils.ts
+++ msn-client/src/app/utils/wink.utils.ts
@@ -4,5 +4,6 @@ export const WINKS : Wink[] = [
     new Wink("assets/images/winks/kiss.gif", "assets/images/winks/kiss.png", "assets/sounds/kiss_wink.mp3",2),
     new Wink("assets/images/winks/laughing_lady.gif", "assets/images/winks/laughing_lady.png", "assets/sounds/laughing_lady.mp3",7),
     new Wink("assets/images/winks/crying_child.gif", "assets/images/winks/crying_child.png", "assets/sounds/crying_child.mp3",6),
+    new Wink("assets/images/winks/worship.gif", "assets/images/winks/worship.png", "assets/sounds/worship.mp3",9),
 ]
         
