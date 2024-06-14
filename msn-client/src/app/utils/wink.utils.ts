a9766a1ea352b91e2f208899104eb0c7d26cb710
Nicholson Rainville Jacques <nics.rj.drawings@gmail.com>
2024-06-13 23:02:22 -0400

    duration water baloon

--- msn-client/src/app/utils/wink.utils.ts
+++ msn-client/src/app/utils/wink.utils.ts
@@ -6,6 +6,6 @@ export const WINKS : Wink[] = [
     new Wink("assets/images/winks/crying_child.gif", "assets/images/winks/crying_child.png", "assets/sounds/crying_child.mp3",6),
     new Wink("assets/images/winks/worship.gif", "assets/images/winks/worship.png", "assets/sounds/worship.mp3",9),
     new Wink("assets/images/winks/toc.gif", "assets/images/winks/toc.png", "assets/sounds/toc.mp3",1),
-    new Wink("assets/images/winks/water_balloon.gif", "assets/images/winks/water_balloon.png", "assets/sounds/water_balloon.mp3",3),
+    new Wink("assets/images/winks/water_balloon.gif", "assets/images/winks/water_balloon.png", "assets/sounds/water_balloon.mp3",2.71),
 ]
         
