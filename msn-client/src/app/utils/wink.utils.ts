94e5afcb2069595fa3c1e4acdc0fac570d1e95cb
Nicholson Rainville Jacques <nics.rj.drawings@gmail.com>
2024-06-14 10:55:58 -0400

    ajout du dancing pig

--- msn-client/src/app/utils/wink.utils.ts
+++ msn-client/src/app/utils/wink.utils.ts
@@ -10,5 +10,6 @@ export const WINKS : Wink[] = [
     new Wink("Water Balloon","assets/images/winks/water_balloon.gif", "assets/images/winks/water_balloon.png", "assets/sounds/water_balloon.mp3",2.71),
     new Wink("Idea","assets/images/winks/bulb.gif", "assets/images/winks/bulb.png", "assets/sounds/bulb.mp3",5),
     new Wink("Boy peeking","assets/images/winks/sideway_man.gif", "assets/images/winks/sideway_man.png", "assets/sounds/sideway_man.mp3",6),
+    new Wink("Dancing pig","assets/images/winks/dancing_pig.gif", "assets/images/winks/dancing_pig.png", "assets/sounds/dancing_pig.mp3",9),
 ]
         
