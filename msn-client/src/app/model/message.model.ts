26acdd7514e5bdfd82372a6f94c03516d3017635
Nicholson Rainville Jacques <nics.rj.drawings@gmail.com>
2024-06-15 00:16:16 -0400

    ajout style model message

--- msn-client/src/app/model/message.model.ts
+++ msn-client/src/app/model/message.model.ts
@@ -6,6 +6,7 @@ export class Message {
         public nomSender: string,
         public type: string,
         public idConversation: number,
+        public style ?: string | null,
         public winkName?: string | null
     ){}
 }
