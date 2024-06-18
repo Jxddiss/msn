98a204d08c9d79c60e55807ef3bbbda68f543403
Nicholson Rainville Jacques <nics.rj.drawings@gmail.com>
2024-06-17 23:47:34 -0400

    détails config

--- msn/src/test/java/com/nicholsonrainville/msn/msn/MsnApplicationTests.java
+++ msn/src/test/java/com/nicholsonrainville/msn/msn/MsnApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nicholsonrainville.examen_film.msn;
+package com.nicholsonrainville.msn.msn;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
