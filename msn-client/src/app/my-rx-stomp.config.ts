f7ef9c29b4ac8ce3726568ad8f5a61dc7824f03e
Nicholson Rainville Jacques <nics.rj.drawings@gmail.com>
2024-06-29 19:02:14 -0400

    setup address backend

--- msn-client/src/app/my-rx-stomp.config.ts
+++ msn-client/src/app/my-rx-stomp.config.ts
@@ -1,7 +1,7 @@
 import { RxStompConfig } from '@stomp/rx-stomp';
 
 export const myRxStompConfig: RxStompConfig = {
-    brokerURL: 'ws://localhost:8080/ws',
+    brokerURL: 'ws://api.nicholsonrj.com/ws',
   
     heartbeatIncoming: 0, 
     heartbeatOutgoing: 0, 
